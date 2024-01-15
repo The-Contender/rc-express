424918230501b18d226ca25c8bf234c8321b4eca
Steve <sohanesian20@gmail.com>
2024-01-15 13:11:01 -0800

    Pscale connect working

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
 const middlewares = require("./middlewares");
 const api = require("./api");
 
@@ -16,8 +19,17 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  // Query to select the first 10 rows from 'properties' table
+  connection.query("SELECT * FROM properties", (err, rows, fields) => {
+    if (err) {
+      // Proper error handling
+      console.error("Error while fetching data: ", err);
+      res.status(500).send("Error while fetching data");
+      return;
+    }
+    // Send the rows as the response
+    //console.log(rows); //update
+    res.send(rows);
   });
 });
 
